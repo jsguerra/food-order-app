ab70e091e1becb81bd1ad518ec5b52839bfdde93
jsguerra <jose@gcostudios.com>
2022-01-05 15:35:08 +0000

    updated app markup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Fragment } from "react";
+import Cart from "./components/Cart/Cart";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      <Cart />
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
